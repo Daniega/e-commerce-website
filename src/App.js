9693c0b3cf55650adc144fb6fbee479b96450e42
Daniega <daniel.gaishuber@gmail.com>
2020-10-20 18:01:08 +0300

    Added Header with Logo and Shop, Contact links

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Switch , Route } from "react-router-dom";
 
+import Header from "./components/header/header.component";
 import HomePage from "./pages/homepage/homepage.component";
 import ShopPage from "./pages/shop/shop.component";
 
@@ -10,6 +11,7 @@ import './App.css';
 function App() {
   return (
     <div>
+      <Header />
       <Switch>
         <Route exact path="/" component={HomePage} />
         <Route path="/shop" component={ShopPage} />
