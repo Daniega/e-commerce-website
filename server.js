8b7a0b3168dc38996ea1138b7d76072f8d1f54d1
Daniega <daniel.gaishuber@gmail.com>
2021-02-22 22:25:47 +0200

    added compression to server.js, for gzip compression in heroku

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const cors = require('cors');
 const bodyParser = require('body-parser');
 const path = require('path');
+const compression = require('compression');
 
 if (process.env.NODE_ENV !== 'production') require('dotenv').config();
 
@@ -10,6 +11,7 @@ const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
 const app = express();
 const port = process.env.PORT || 5000;
 
+app.use(compression());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
@@ -18,7 +20,7 @@ app.use(cors());
 if (process.env.NODE_ENV === 'production') {
 	app.use(express.static(path.join(__dirname, 'client/build')));
 
-	app.get('*', function(req, res) {
+	app.get('*', function (req, res) {
 		res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
 	});
 }
@@ -32,9 +34,9 @@ app.listen(port, (error) => {
 
 app.post('/payment', (req, res) => {
 	const body = {
-		source   : req.body.token.id,
-		amount   : req.body.amount,
-		currency : 'usd'
+		source: req.body.token.id,
+		amount: req.body.amount,
+		currency: 'usd',
 	};
 
 	stripe.charges.create(body, (stripeErr, stripeRes) => {
