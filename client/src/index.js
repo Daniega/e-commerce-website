9449803f8fdd55b08c66b2e30375c7a82eb0ec98
Daniega <daniel.gaishuber@gmail.com>
2021-03-08 18:05:01 +0200

    added service worker and sslify to make the application PWA (progressive web app)

--- client/src/index.js
+++ client/src/index.js
@@ -1,24 +1,26 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter } from "react-router-dom";
-import { Provider } from "react-redux";
-
-import { PersistGate } from "redux-persist/integration/react";
-
-import {store, persistor} from "./redux/store";
+//router
+import { BrowserRouter } from 'react-router-dom';
+//redux
+import { store, persistor } from './redux/store';
+import { PersistGate } from 'redux-persist/integration/react';
+import { Provider } from 'react-redux';
+//PWA - Progressive Web App - Service Worker
+import * as serviceWorker from './serviceWorker';
 
 import './index.css';
 import App from './App';
 
-
 ReactDOM.render(
-    <Provider store={store}>
-        <BrowserRouter>
-            <PersistGate persistor={persistor}>
-                <App />
-            </PersistGate>
-        </BrowserRouter>
-    </Provider>
-    ,
-    document.getElementById('root'));
+	<Provider store={store}>
+		<BrowserRouter>
+			<PersistGate persistor={persistor}>
+				<App />
+			</PersistGate>
+		</BrowserRouter>
+	</Provider>,
+	document.getElementById('root')
+);
 
+serviceWorker.register();
