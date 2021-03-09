9e93114006c01388704ce8c16dea5fa63f48b54f
Daniega <daniel.gaishuber@gmail.com>
2021-03-09 10:18:00 +0200

    removed compression from server.js

--- server.js
+++ server.js
@@ -3,7 +3,6 @@ const cors = require('cors');
 const bodyParser = require('body-parser');
 const path = require('path');
 const compression = require('compression');
-const enforce = require('express-sslify');
 
 if (process.env.NODE_ENV !== 'production') require('dotenv').config();
 
@@ -12,13 +11,11 @@ const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
 const app = express();
 const port = process.env.PORT || 5000;
 
-app.use(compression);
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cors());
 
 if (process.env.NODE_ENV === 'production') {
-	app.use(compression);
 	app.use(express.static(path.join(__dirname, 'client/build')));
 	app.get('*', function (req, res) {
 		res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
