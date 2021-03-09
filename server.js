e7dc80637de9a1cced67e9dd5776789991864494
Daniega <daniel.gaishuber@gmail.com>
2021-03-09 09:58:35 +0200

    updated service worker code in server.js

--- server.js
+++ server.js
@@ -13,30 +13,28 @@ const app = express();
 const port = process.env.PORT || 5000;
 
 app.use(compression());
+app.use(enforce.HTTPS({ trustProtoHeader: true }));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(enforce.HTTPS({ trustProtoHeader: true }));
 app.use(cors());
 
 if (process.env.NODE_ENV === 'production') {
+	app.use(compression);
+	app.use(enforce.HTTPS({ trustProtoHeader: true }));
 	app.use(express.static(path.join(__dirname, 'client/build')));
-	app.get('/service-worker.js', (req, res) => {
-		res.sendFile(path.resolve(__dirname, '..', 'build', 'serviceWorker.js'));
-	});
+
 	app.get('*', function (req, res) {
 		res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
 	});
 }
 
 app.listen(port, (error) => {
-	if (error) {
-		throw error;
-	}
+	if (error) throw error;
 	console.log('Server running on port: ' + port);
 });
 
 app.get('/service-worker.js', (req, res) => {
-	res.sendFile(path.resolve(__dirname, '..', 'build', 'serviceWorker.js'));
+	res.sendFile(path.resolve(__dirname, '..', 'build', 'service-worker.js'));
 });
 
 app.post('/payment', (req, res) => {
@@ -54,5 +52,3 @@ app.post('/payment', (req, res) => {
 		}
 	});
 });
-
-//TODO move post method to controller
