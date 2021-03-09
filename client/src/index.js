d25f864b7cc54cefe5e5163ef53dafb5c82a19ac
Daniega <daniel.gaishuber@gmail.com>
2021-03-09 10:07:57 +0200

    removed service worker from app

--- client/src/index.js
+++ client/src/index.js
@@ -6,8 +6,6 @@ import { BrowserRouter } from 'react-router-dom';
 import { store, persistor } from './redux/store';
 import { PersistGate } from 'redux-persist/integration/react';
 import { Provider } from 'react-redux';
-//PWA - Progressive Web App - Service Worker
-import * as serviceWorker from './serviceWorker';
 
 import './index.css';
 import App from './App';
@@ -22,5 +20,3 @@ ReactDOM.render(
 	</Provider>,
 	document.getElementById('root')
 );
-
-serviceWorker.register();
--- server.js
+++ server.js
@@ -12,15 +12,11 @@ const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
 const app = express();
 const port = process.env.PORT || 5000;
 
-app.use(compression());
-app.use(enforce.HTTPS({ trustProtoHeader: true }));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cors());
 
 if (process.env.NODE_ENV === 'production') {
-	app.use(compression);
-	app.use(enforce.HTTPS({ trustProtoHeader: true }));
 	app.use(express.static(path.join(__dirname, 'client/build')));
 
 	app.get('*', function (req, res) {
@@ -31,10 +27,6 @@ if (process.env.NODE_ENV === 'production') {
 app.listen(port, (error) => {
 	if (error) throw error;
 	console.log('Server running on port: ' + port);
-});
-
-app.get('/service-worker.js', (req, res) => {
-	res.sendFile(path.resolve(__dirname, '..', 'build', 'service-worker.js'));
 });
 
 app.post('/payment', (req, res) => {
