9e338f9b7928af914aa14ed7a1801e39fef40e2e
Daniega <daniel.gaishuber@gmail.com>
2021-03-10 16:40:18 +0200

    text refactoring

--- client/src/redux/cart/cart.selectors.js
+++ client/src/redux/cart/cart.selectors.js
@@ -1,23 +1,18 @@
-import { createSelector } from "reselect";
+import { createSelector } from 'reselect';
 
-const selectCart = state => (state.cart);
-
-export const selectCartItems = createSelector(
-    [selectCart],
-    (cart) => cart.cartItems
+const selectCart = (state) => state.cart;
+//select all cart items
+export const selectCartItems = createSelector([selectCart], (cart) => cart.cartItems);
+//hide or show cart-dropdown component
+export const selectCartHidden = createSelector([selectCart], (cart) => cart.hidden);
+//count number of items in cart
+export const selectCartItemsCount = createSelector([selectCartItems], (cartItems) =>
+	cartItems.reduce((accumulatedQuantity, cartItem) => accumulatedQuantity + cartItem.quantity, 0)
 );
-
-export const selectCartHidden = createSelector(
-    [selectCart],
-    (cart) => cart.hidden
-);
-
-export const selectCartItemsCount = createSelector(
-    [selectCartItems],
-    cartItems => cartItems.reduce((accumulatedQuantity, cartItem) => accumulatedQuantity + cartItem.quantity ,0)
-);
-
-export const selectCartTotal = createSelector(
-    [selectCartItems],
-    cartItems => cartItems.reduce((accumulatedQuantity, cartItem) => accumulatedQuantity + cartItem.quantity * cartItem.price,0)
+//count total sum of items in cart
+export const selectCartTotal = createSelector([selectCartItems], (cartItems) =>
+	cartItems.reduce(
+		(accumulatedQuantity, cartItem) => accumulatedQuantity + cartItem.quantity * cartItem.price,
+		0
+	)
 );
